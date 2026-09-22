b9290751009d87f5b6b09b753733eda66dd0224d
ThomasPeccavet <peccavetthomas@gmail.com>
2026-09-22 15:53:52 +0200

    B135.20: label hot-map native seed build

--- 3ds/source/main.c
+++ 3ds/source/main.c
@@ -15062,7 +15062,7 @@ int main(void)
             FMDmaDebugStats b130_dma = {0};
             fm_memory_dma_debug(&b130_dma);
 
-            printf("BUILD B135.19-INTERP-REGION (BASE B131)\n");
+            printf("BUILD B135.20-HOT-MAP-SEEDS (BASE B131)\n");
 
             printf(
                 "RUN:%c F:%lu CPU:%08lX MENU:%u\n",
