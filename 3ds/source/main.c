aa5e9e6a0936ac6d69562ec812dfe434cc9adb7a
ThomasPeccavet <peccavetthomas@gmail.com>
2026-09-23 13:10:06 +0200

    B135.31: label flat-rectangle fast path

--- 3ds/source/main.c
+++ 3ds/source/main.c
@@ -15156,7 +15156,7 @@ int main(void)
             FMDmaDebugStats b130_dma = {0};
             fm_memory_dma_debug(&b130_dma);
 
-            printf("BUILD B135.30-GPU-SAMPLE (BASE B131)\n");
+            printf("BUILD B135.31-FAST-R62 (BASE B131)\n");
 
             printf(
                 "RUN:%c F:%lu CPU:%08lX MENU:%u\n",
