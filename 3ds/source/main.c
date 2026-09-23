d7bd360174ac4c8938f01033c9efffd91617609b
ThomasPeccavet <peccavetthomas@gmail.com>
2026-09-23 11:00:06 +0200

    B135.28: label 34D30 host alias test

--- 3ds/source/main.c
+++ 3ds/source/main.c
@@ -15156,7 +15156,7 @@ int main(void)
             FMDmaDebugStats b130_dma = {0};
             fm_memory_dma_debug(&b130_dma);
 
-            printf("BUILD B135.27-ENTRY-DIAG (BASE B131)\n");
+            printf("BUILD B135.28-34D30-HOST (BASE B131)\n");
 
             printf(
                 "RUN:%c F:%lu CPU:%08lX MENU:%u\n",
