733ebc3235076c4786498edd3a0134c5b0c621b4
ThomasPeccavet <peccavetthomas@gmail.com>
2026-09-23 08:36:20 +0200

    B135.22: extend chained probe to a third resident region

--- 3ds/include/fm_runtime_shim.h
+++ 3ds/include/fm_runtime_shim.h
@@ -129,6 +129,8 @@ FMRuntimeProbeResult fm_runtime_probe_chain(
     uint32_t phys_end,
     uint32_t phys2_begin,
     uint32_t phys2_end,
+    uint32_t phys3_begin,
+    uint32_t phys3_end,
     uint32_t max_dispatches,
     uint32_t *out_dispatches
 );
