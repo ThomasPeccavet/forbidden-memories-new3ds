27106de432dff92c13ed9ee724f8c76360eb9dc6
ThomasPeccavet <peccavetthomas@gmail.com>
2026-09-24 10:20:56 +0200

    B135.45: label generic textured-quad fidelity test

--- 3ds/source/main.c
+++ 3ds/source/main.c
@@ -15263,7 +15263,7 @@ int main(void)
             FMDmaDebugStats b130_dma = {0};
             fm_memory_dma_debug(&b130_dma);
 
-            printf("BUILD B135.44-FOLLOW-DRAWBUF (BASE B131)\n");
+            printf("BUILD B135.45-GENERIC-2C (BASE B131)\n");
 
             printf(
                 "RUN:%c F:%lu CPU:%08lX MENU:%u\n",
@@ -15771,9 +15771,10 @@ int main(void)
                         );
 
                         printf(
-                            "FOLLOW n/x:%lu/%lu\n",
+                            "FOLLOW n/x:%lu/%lu QGEN:%lu\n",
                             (unsigned long)g_b13544_follow_drawbuf,
-                            (unsigned long)g_b13544_last_draw_x
+                            (unsigned long)g_b13544_last_draw_x,
+                            (unsigned long)fm_gpu_b13545_generic_texquads()
                         );
                     }
 
